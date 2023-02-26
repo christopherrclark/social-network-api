64a1cb587644cc147f4a02eee23f66b84ae9f53e
Fozzy Yeti <christopherrclark@gmail.com>
2023-02-25 23:14:54 -0600

    finished userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,8 @@
 const { User, Thought } = require('../models');
+
 const userController = {
+
+  // get all users
   getAllUsers(req,res){
     User.find()
     .then(function(users)
@@ -11,6 +14,8 @@ const userController = {
       res.status(404).json(err);
     })
   },
+
+  // get single user
   getSingleUser(req,res){
     User.findOne({_id:req.params.userId})
     .then(function(user)
@@ -25,6 +30,8 @@ const userController = {
       res.status(404).json({message: err})
     })
   },
+
+  // create a new user
   createUser(req,res){
     User.create(req.body)
     .then(function(user){
@@ -34,7 +41,75 @@ const userController = {
       console.error(err)
       res.status(404).json({message: err})
     })
+  },
+
+  // update user
+  updateUser(req,res){
+    User.findByIdAndUpdate(req.params.userId, req.body, {new:true})
+    .then(function(user){
+      if (!user){
+        return res.status(404).json({message: "no such user"})
+      }
+      res.json(user);
+    })
+    .catch(function(err){
+      console.error(err)
+      res.status(404).json({message: err})
+    })
+  },
+
+  // delete user
+  deleteUser(req,res){
+    User.findByIdAndDelete(req.params.userId)
+    .then(function(user){
+      if (!user){
+        return res.status(404).json({message: "no such user"})
+      }
+      res.json({message: "user deleted"});
+    })
+    .catch(function(err){
+      console.error(err)
+      res.status(404).json({message: err})
+    })
+  },
+
+  // add friend
+  addFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        if (!updatedUser) {
+          return res.status(404).json({message: "no such user"});
+        }
+        res.json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(404).json({ message: err });
+      });
+  },
+
+  // delete friend
+  deleteFriend(req, res) {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: "no such user" })
+        }
+        res.json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(404).json({ message: err })
+      });
   }
-}
+};
 
 module.exports = userController
